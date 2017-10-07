2131f88a7327595acc080ed5e7067dfa0274e6ea
GRicha2380 <gregor.richardson@gmail.com>
2017-10-06 20:09:59 -0700

    starting FTP config

--- gulpfile.js
+++ gulpfile.js
@@ -8,6 +8,67 @@ var gulp = require('gulp'),
 	map = require('gulp-sourcemaps');
 	del = require('del'),
 	browserSync = require('browser-sync').create();
+	var gutil = require( 'gulp-util' );
+	var ftp = require( 'vinyl-ftp' );
+
+// FTP Info
+var user = process.env.FTP_USER;
+var password = process.env.FTP_PWD;
+var host = '173.201.63.1';
+var port = 21;
+var localFilesGlob = ['./**/*'];
+var remoteFolder = '/gregor'
+
+// helper function to build an FTP connection based on our configuration
+function getFtpConnection() {
+    return ftp.create({
+        host: host,
+        port: port,
+        user: user,
+        password: password,
+        parallel: 5,
+        log: gutil.log
+    });
+}
+
+/**
+ * Deploy task.
+ * Copies the new files to the server
+ *
+ * Usage: `FTP_USER=someuser FTP_PWD=somepwd gulp ftp-deploy`
+ */
+gulp.task('ftp-deploy', function() {
+
+    var conn = getFtpConnection();
+
+    return gulp.src(localFilesGlob, { base: '.', buffer: false })
+        .pipe( conn.newer( remoteFolder ) ) // only upload newer files
+        .pipe( conn.dest( remoteFolder ) )
+    ;
+});
+
+/**
+ * Watch deploy task.
+ * Watches the local copy for changes and copies the new files to the server whenever an update is detected
+ *
+ * Usage: `FTP_USER=someuser FTP_PWD=somepwd gulp ftp-deploy-watch`
+ */
+gulp.task('ftp-deploy-watch', function() {
+
+    var conn = getFtpConnection();
+
+    gulp.watch(localFilesGlob)
+    .on('change', function(event) {
+      console.log('Changes detected! Uploading file "' + event.path + '", ' + event.type);
+
+      return gulp.src( [event.path], { base: '.', buffer: false } )
+        .pipe( conn.newer( remoteFolder ) ) // only upload newer files
+        .pipe( conn.dest( remoteFolder ) )
+      ;
+    });
+});
+
+// END FTP
 
 // Static Server + watching scss/html files
 gulp.task('serve', ['css'], function() {
